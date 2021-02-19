521e50494adb119edd01110829d3c46e32f09b67
Dmitriy Uchitel <78314898+unitedfan1986@users.noreply.github.com>
2021-02-19 17:29:45 -0500

    Update homework_functions.py

--- homework_functions.py
+++ homework_functions.py
@@ -1,1 +1,10 @@
+numbers = ([1, 1, 2, 4, 4])
+def get_unique_elements(numbers):
+    get_unique_elements = []
 
+    unique_elements = set(numbers)
+
+    for numbers in unique_elements:
+        get_unique_elements.append(numbers)
+    return get_unique_elements
+print(get_unique_elements(numbers))
